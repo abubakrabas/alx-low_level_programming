68aa80dce28ff21ddd0edc85a360469324313d54
abubakrabas <princebakry@gmail.com>
2022-10-12 21:52:56 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#1/bin/bash
+#include <stdio.h>
+/**
+ *main - Printing
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
