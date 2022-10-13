10c883b0b684937101889855792a91a2c26e622a
abubakrabas <princebakry@gmail.com>
2022-10-12 22:09:19 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *main - Printing
- *
- * Return: Always 0(Success)
- */
+* main - Printing
+*
+* Return: Always 0 (Successful)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual
+puzzle");
+return (0);
 }
