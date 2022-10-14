1b329d4fe734d943f498e9a7debd86916285f67e
abubakrabas <princebakry@gmail.com>
2022-10-14 12:57:47 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description:printinggg single digits numbers of base 10 starting from 01
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
